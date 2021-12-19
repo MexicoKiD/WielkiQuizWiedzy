7062c25a12d3fbcf64fb0c0687b2bdd93d2dbc0a
MexicoKiD <hmocicki@gmail.com>
2021-12-19 20:05:58 +0100

    splash screen

---
+++ WielkiQuizWiedzy/SplashScreenActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace WielkiQuizWiedzy
+{
+    [Activity(Label = "SplashScreenActivity", MainLauncher =true, Theme ="@style/Theme.Splash", NoHistory = true, Icon = "@drawable/splash")]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //Display Splash Screen for 4 Sec  
+            Thread.Sleep(4000);
+            //Start Activity1 Activity  
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
